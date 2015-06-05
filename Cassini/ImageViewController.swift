0b4fad98b06c8e0e6313e87553f8f31e85a50c66
brbatwork <bill.barbour@oracle.com>
2015-06-05 10:42:13 -0400

    Added multi-threadedness

--- Cassini/ImageViewController.swift
+++ Cassini/ImageViewController.swift
@@ -28,6 +28,8 @@ class ImageViewController: UIViewController, UIScrollViewDelegate {
         }
     }
     
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
     func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
         return imageView
     }
@@ -40,18 +42,31 @@ class ImageViewController: UIViewController, UIScrollViewDelegate {
             imageView.image = newValue
             imageView.sizeToFit()
             scrollView?.contentSize = imageView.frame.size
+            spinner?.stopAnimating()
         }
     }
     
     private func fetchImage() {
         if let url = imageURL {
-            let imageData = NSData(contentsOfURL: url) //Blocking out to network!
+            spinner?.startAnimating()
+            let qos = Int(QOS_CLASS_USER_INITIATED.value)
             
-            if imageData != nil {
-                image = UIImage(data: imageData!)
-            } else {
-                image = nil
-            }
+            dispatch_async(dispatch_get_global_queue(qos, 0), { () -> Void in
+                let imageData = NSData(contentsOfURL: url) //Blocking out to network!
+                
+                //All updates to the UI must be on the main queue
+                dispatch_async(dispatch_get_main_queue()) {
+                    //Update the UI if the url is the same as they requested
+                    if url == self.imageURL {
+                        if imageData != nil {
+                            self.image = UIImage(data: imageData!)
+                        } else {
+                            self.image = nil
+                        }
+                    }
+                }
+            })
+
         }
     }
     
